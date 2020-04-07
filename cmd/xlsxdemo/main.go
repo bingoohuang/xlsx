4598d145c4805afeefcc5eeef0412c449c6878a2
bingoohuang <bingoo.huang@gmail.com>
2020-04-07 14:59:59 +0800

    demo

---
+++ cmd/xlsxdemo/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/bingoohuang/xlsx"
+)
+
+type RegisterTable struct {
+	ContactName  string    // 联系人
+	Mobile       string    // 手机
+	Landline     string    // 座机
+	RegisterDate time.Time // 登记日期
+	DeviceType   string    `placeholderCell:"C9"` // 类型
+	Manufacturer string    // 生产厂家
+	DeviceModern string    // 型号
+}
+
+// nolint gochecknoinits
+func init() {
+	flag.String("test.v", "true", "test.v")
+}
+
+func main() {
+	x, _ := xlsx.New(xlsx.WithTemplatePlaceholder("testdata/placeholder.xlsx"))
+	defer x.Close()
+
+	_ = x.Write(RegisterTable{
+		ContactName:  "隔壁老王",
+		Mobile:       "1234567890",
+		Landline:     "010-1234567890",
+		RegisterDate: time.Now(),
+		DeviceType:   "A1",
+		Manufacturer: "来弄你",
+		DeviceModern: "X786",
+	})
+
+	_ = x.SaveToFile("out_placeholder.xlsx")
+}

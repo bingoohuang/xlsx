42f5282bb839aeb1ef3543d3f11a80ccdfd78efd
bingoohuang <bingoo.huang@gmail.com>
2022-12-21 14:28:25 +0800

    merge example cmd

--- cmd/xlsx/main.go
+++ cmd/xlsx/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/bingoohuang/xlsx"
 	"github.com/unidoc/unioffice/spreadsheet"
 )
 
 func main() {
+	var demo string
+	flag.StringVar(&demo, "demo", "", "demo for what: placeholder")
+	flag.Parse()
+	switch demo {
+	case "placeholder", "ph":
+		placeholder()
+	default:
+		readCellValues()
+	}
+
+}
+
+func readCellValues() {
 	fn := ""
 
 	if len(os.Args) <= 1 {
@@ -40,4 +55,35 @@ func main() {
 	}
 
 	log.Printf("evaluated %d formulas from %s sheet", formulaCount, fn)
+}
+
+// RegisterTable 注册登记表信息.
+type RegisterTable struct {
+	ContactName  string    `asPlaceholder:"true"` // 联系人
+	Mobile       string    // 手机
+	Landline     string    // 座机
+	RegisterDate time.Time `format:"yyyy-MM-dd"`  // 登记日期
+	DeviceType   string    `placeholderCell:"C9"` // 类型
+	Manufacturer string    // 生产厂家
+	DeviceModern string    // 型号
+}
+
+func placeholder() {
+	template := "testdata/placeholder.xlsx"
+	x, _ := xlsx.New(xlsx.WithTemplate(template))
+	defer x.Close()
+
+	_ = x.Write(RegisterTable{
+		ContactName:  "隔壁老王",
+		Mobile:       "1234567890",
+		Landline:     "010-1234567890",
+		RegisterDate: time.Now(),
+		DeviceType:   "A1",
+		Manufacturer: "来弄你",
+		DeviceModern: "X786",
+	})
+
+	out := "out_placeholder.xlsx"
+	_ = x.SaveToFile(out)
+	log.Printf("tempalte: %s, outout excel: %s", template, out)
 }
